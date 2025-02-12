3fc81559d832425e65233492bb4d60e401d27197
itscharlesm <itscharlesm@gmail.com>
2025-02-12 18:45:30 +0800

    vmg done

--- resources/views/main/about/about.blade.php
+++ resources/views/main/about/about.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.themes.main')
 @section('content')
-<br><br><br><br><br>
+    <br><br><br><br><br>
     <!-- About -->
     <section class="section bg-grey">
         <div class="container-fluid px-5">
@@ -8,26 +8,29 @@
                 <!-- First Section -->
                 <div class="col-lg-12">
                     <div class="section-title">
-                        <h2 class="mb-3">About CMC</h2>
+                        <h2 class="mb-3">About RPC</h2>
                         <hr>
                     </div>
                     <p class="font-weight-light text-justify">
-                        The University of Mindanao acknowledges the vital role of research as a catalyst in the promotion of instruction and extension in higher education. As such, it focuses its strategic direction on the development of a strong research culture that would enable its faculty, staff and students to be engaged in research that is responsive to the local, regional and national development needs.
+                        The University of Mindanao acknowledges the vital role of research as a catalyst in the promotion of
+                        instruction and extension in higher education. As such, it focuses its strategic direction on the
+                        development of a strong research culture that would enable its faculty, staff and students to be
+                        engaged in research that is responsive to the local, regional and national development needs.
                     </p>
                     <p class="font-weight-light text-justify">
-                        The Research and Publication Center (RPC) was established to manage and implement the research program of the University. It provides inputs to administrators for policy and/or decision-making through conduct of analyses and evaluations of programs and projects. It spearheads capacity development activities aimed at building and upgrading the research skills of faculty, staff and students. It exercises monitoring and facilitating functions for the research projects of the University. It aims to upscale the horizon of knowledge generation and utilization in the sphere where the University of Mindanao is significantly contributing as an academic institution and as a kernel of new knowledge.
+                        The Research and Publication Center (RPC) was established to manage and implement the research
+                        program of the University. It provides inputs to administrators for policy and/or decision-making
+                        through conduct of analyses and evaluations of programs and projects. It spearheads capacity
+                        development activities aimed at building and upgrading the research skills of faculty, staff and
+                        students. It exercises monitoring and facilitating functions for the research projects of the
+                        University. It aims to upscale the horizon of knowledge generation and utilization in the sphere
+                        where the University of Mindanao is significantly contributing as an academic institution and as a
+                        kernel of new knowledge.
                     </p>
                 </div>
 
                 <!-- Second Section -->
                 <div class="col-lg-12">
-                    <div class="section-title">
-                        <h2 class="mb-3">Core Competencies</h2>
-                        <p>
-                            Quality, affordable, and open education
-                        </p>
-                    </div>
-
                     <!-- Accordion Section -->
                     <div class="accordion w-100" id="accordionExample">
                         <div class="card">
@@ -39,12 +42,12 @@
                                     </button>
                                 </h2>
                             </div>
-
                             <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
                                 data-parent="#accordionExample">
                                 <div class="card-body">
-                                    <p>
-                                        A leading research institution recognized for its significant contribution in the generation of new knowledge for the improvement of the quality of human life.
+                                    <p class="text-justify">
+                                        A leading research institution recognized for its significant contribution in the
+                                        generation of new knowledge for the improvement of the quality of human life.
                                     </p>
                                 </div>
                             </div>
@@ -63,71 +66,94 @@
                             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
                                 data-parent="#accordionExample">
                                 <div class="card-body">
-                                    <p>
-                                        Central Mindanao Colleges is an academic institution PROVIDING quality instruction.
-                                    </p>
-                                    <p>
-                                        DEVELOPING innovative researchers and ENGAGING in responsive extensions program;
-                                    </p>
-                                    <p>
-                                        That is committed to produce competent and socially responsible individuals who can
-                                        carry out responsibilities for local and global communities.
+                                    <p class="text-justify">
+                                        To nurture research culture among faculty, staff, and students demonstrating the
+                                        love for truth and upholding the values of rigor, artistry and ethics.
                                     </p>
                                 </div>
                             </div>
                         </div>
-
                         <div class="card mt-2">
                             <div class="card-header " id="headingThree">
                                 <h2 class="mb-0">
                                     <button class="btn btn-link btn-block text-left collapsed" type="button"
                                         data-toggle="collapse" data-target="#collapseThree" aria-expanded="false"
                                         aria-controls="collapseThree">
-                                        Values
+                                        Goals
                                     </button>
                                 </h2>
                             </div>
                             <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
                                 data-parent="#accordionExample">
                                 <div class="card-body">
-                                    <p>
-                                        CMC is committed to attain its vision and mission guided by the following values:
-                                    <ul>
-                                        <li>Love for God</li>
-                                        <li>Integrity</li>
-                                        <li>Family orientation</li>
-                                        <li>Excellence</li>
+                                    <p class="text-justify">
+                                    <ul style="list-style-type: disc;">
+                                        <li>To develop skilled, committed, conscientious, and prudent researchers.</li>
+                                        <li>To upgrade research-support mechanisms such as facilities, devices, policies,
+                                            and systems.</li>
+                                        <li>To produce, present, publish and practice researches that respond to the needs
+                                            of the time and contribute to the sustainable development of society.</li>
+                                        <li>To establish and sustain strategic linkages with external agencies for purposes
+                                            of collaboration and funding.</li>
                                     </ul>
                                     </p>
-
                                 </div>
                             </div>
                         </div>
-
                         <div class="card mt-2">
                             <div class="card-header" id="headingFour">
                                 <h2 class="mb-0">
                                     <button class="btn btn-link btn-block text-left collapsed" type="button"
                                         data-toggle="collapse" data-target="#collapseFour" aria-expanded="false"
                                         aria-controls="collapseFour">
-                                        Goals
+                                        Objectives
                                     </button>
                                 </h2>
                             </div>
                             <div id="collapseFour" class="collapse" aria-labelledby="headingFour"
                                 data-parent="#accordionExample">
                                 <div class="card-body">
-                                    <p>
-                                        CMC is committed to attain its vision and mission guided by the following goals:
-                                    <ol style="list-style: decimal;">
-                                        <li>Produce competent graduates that epitomize the values of the institution</li>
-                                        <li>Foster innovation in all academic programs</li>
-                                        <li>Increase linkages and connections</li>
-                                        <li>Provide an enabling environment to intensify employee productivity</li>
-                                        <li>Secure financial future</li>
-                                    </ol>
+                                    <p class="text-justify">
+                                        <strong>For the development of researcher competence:</strong>
+                                    <ul style="list-style-type: disc;">
+                                        <li>To initiate educational activities such as trainings and immersion aimed at
+                                            building/enhancing skills, commitment, conscientiousness and prudence in
+                                            research.</li>
+                                        <li>To implement a recognition/reward system that acclaims outstanding research
+                                            performances and achievements.</li>
+                                        <li>To participate in research fora and conferences with the intention of updating
+                                            knowledge and expertise.</li>
+                                    </ul>
                                     </p>
-
+                                    <p class="text-justify">
+                                        <strong>For the advancement of research support mechanisms:</strong>
+                                    <ul style="list-style-type: disc;">
+                                        <li>To assess and improve policies and systems towards facilitation of effective
+                                            conduct of research.</li>
+                                        <li>To acquire new and innovative facilities and devices needed in research
+                                            undertakings.</li>
+                                    </ul>
+                                    </p>
+                                    <p class="text-justify">
+                                        <strong>For the generation, dissemination, publication and utilization of relevant
+                                            researches:</strong>
+                                    <ul style="list-style-type: disc;">
+                                        <li>To conduct researches in accordance with the University research agenda.</li>
+                                        <li>To provide for the dissemination of research findings through public lectures,
+                                            fora and paper presentations.</li>
+                                        <li>To publish research findings for utilization of stakeholders.</li>
+                                    </ul>
+                                    </p>
+                                    <p class="text-justify">
+                                        <strong>For the establishment and sustainability of strategic linkages:</strong>
+                                    <ul style="list-style-type: disc;">
+                                        <li>To join and be involved in research-related professional organizations.</li>
+                                        <li>To engage in consultancy services that are compatible to the University’s
+                                            research agenda and expertise.</li>
+                                        <li>To establish research partnership with external organizations.</li>
+                                        <li>To avail of local, national and international research grants.</li>
+                                    </ul>
+                                    </p>
                                 </div>
                             </div>
                         </div>
