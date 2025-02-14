aadf5a1b4487387768fe7c717d122b08421b3063
itscharlesm <itscharlesm@gmail.com>
2025-02-14 13:30:51 +0800

    linkages done

--- resources/views/main/linkage/linkages.blade.php
+++ resources/views/main/linkage/linkages.blade.php
@@ -5,57 +5,115 @@
             <!-- Title -->
             <div class="content-header text-center" data-aos="zoom-in-left" data-aos-duration="1000" data-aos-delay="100"
                 data-aos-once="false" style="margin-top: 100px;">
-                <x-content-header title="DOWNLOADS" />
+                <x-content-header title="LINKAGES" />
             </div>
             <!-- Breadcrumb -->
             <x-breadcrumb :items="[
                 ['label' => 'Home', 'url' => url('/'), 'active' => false],
-                ['label' => 'Downloads', 'url' => '#', 'active' => true],
+                ['label' => 'Linkages', 'url' => '#', 'active' => true],
             ]" />
             <div class="row">
                 <!-- Attachments -->
                 <div class="col-lg-12">
                     <div class="section-title">
-                        <h2 class="mb-3">Important Attachments</h2>
+                        <h2 class="mb-3">INTERNATIONAL LINKAGES</h2>
                         <ul style="list-style-type: disc; padding-left: 20px;">
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Certificate of Completed Title Defense.pdf') }}"
-                                    target="_blank">Certificate of Completed Title Defense.pdf</a></li>
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Professional Schools - Research Presentation and Evaluation - Title.pdf') }}"
-                                    target="_blank">Professional Schools - Research Presentation and Evaluation -
-                                    Title.pdf</a></li>
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Research Final Defense Evaluation Form - RPC.pdf') }}"
-                                    target="_blank">Research Final Defense Evaluation Form - RPC.pdf</a></li>
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Research Outline Evaluation Form - RPC.pdf') }}"
-                                    target="_blank">Research Outline Evaluation Form - RPC.pdf</a></li>
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Research Personnel Evaluation Form.pdf') }}"
-                                    target="_blank">Research Personnel Evaluation Form.pdf</a></li>
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Research Ranking Form.pdf') }}" target="_blank">Research
-                                    Ranking Form.pdf</a></li>
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Research Title Evaluation Sheet.pdf') }}"
-                                    target="_blank">Research Title Evaluation Sheet.pdf</a></li>
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Title Defense Guide.pdf') }}" target="_blank">Title
-                                    Defense Guide.pdf</a></li>
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Thesis & Capstone I - Title and Outline Evaluation Form.pdf') }}"
-                                    target="_blank">Thesis & Capstone I - Title and Outline Evaluation Form.pdf</a></li>
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Thesis & Capstone II - Evaluation Form.pdf') }}"
-                                    target="_blank">Thesis & Capstone II - Evaluation Form.pdf</a></li>
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Thesis Final Defense Evaluation Form - For Students.pdf') }}"
-                                    target="_blank">Thesis Final Defense Evaluation Form - For Students.pdf</a></li>
-                            <li style="margin-bottom: 10px;"><a
-                                    href="{{ asset('images/download/Thesis Outline Defense Evaluation Form - For Students.pdf') }}"
-                                    target="_blank">Thesis Outline Defense Evaluation Form - For Students.pdf</a></li>
-                        </ul>
+                            <li style="margin-bottom: 10px;">
+                                <p>Australia Awards (Australia Global Alumni), Australia</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Biomass Japan, Japan</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>California Academy of Science, USA</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Daugavpils University, Latvia</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>National Geographic, Inc., Washington, USA</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Save the Children International & Action Against Hunger International</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Canada Museum of Nature, Canada</p>
+                            </li>
+                        </ul>                        
+                    </div>
+                    <div class="section-title">
+                        <h2 class="mb-3">LOCAL AND NATIONAL LINKAGES</h2>
+                        <ul style="list-style-type: disc; padding-left: 20px;">
+                            <li style="margin-bottom: 10px;">
+                                <p>Commission on Higher Education</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Davao Intellectual Property Society</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Davao Colleges and Universities Network (DACUN)</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Davao City Water District (DCWD)</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Davao Region Industry Energy and Engineering Research and Development Consortium (DRIEERDC)</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Davao Oriental State College of Science and Technology</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Department of Interior and Local Government</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Department of Environment and Natural Resources Office XI</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Department of Science and Technology XI</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Franklin Baker</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Research Association of Colleges and Universities in Region XI (RACUR XI)</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Idealsearch Consulting Company</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>IPOPHIL</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Konrad Adenauer Stiftung - Democratic Leadership and Active Civil Society Employment</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Mindanao Development Authority</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Nippon Steel & Sumikin Engineering Co. Ltd.</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Protected Area Management Board of Mt. Hamiguitan Range Wildlife and Sanctuary</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Peace and Equity Foundation</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Philippine National Police - Special Action Force</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>PNP RHS-XI</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Sagittarius Mines, Incorporated</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Sultan Kudarat State University, Sultan Kudarat</p>
+                            </li>
+                            <li style="margin-bottom: 10px;">
+                                <p>Western Mindanao State University, Zamboanga City</p>
+                            </li>
+                        </ul>                        
                     </div>
                 </div>
             </div>
