6f73beeecd60082671e96a985c2f6e0b859babbf
itscharlesm <itscharlesm@gmail.com>
2025-02-14 14:07:42 +0800

    Coleoptera frontend done backend soon

--- resources/views/main/center/coleoptera.blade.php
+++ resources/views/main/center/coleoptera.blade.php
@@ -5,23 +5,20 @@
             <!-- Title -->
             <div class="content-header text-center" data-aos="zoom-in-left" data-aos-duration="1000" data-aos-delay="100"
                 data-aos-once="false" style="margin-top: 100px;">
-                <x-content-header title="CGNIES" />
+                <x-content-header title="COLEOPTERA RESEARCH CENTER" />
             </div>
             <!-- Breadcrumb -->
             <x-breadcrumb :items="[
                 ['label' => 'Home', 'url' => url('/'), 'active' => false],
                 ['label' => 'Centers', 'url' => '#', 'active' => true],
-                [
-                    'label' => 'Green Nanotechnology Innovations for Environmental Solutions (CGNIES)',
-                    'url' => '#',
-                    'active' => true,
+                ['label' => 'Coleoptera', 'url' => '#', 'active' => true,
                 ],
             ]" />
             <div class="row">
                 <!-- Attachments -->
                 <div class="col-lg-12">
                     <div class="section-title">
-                        <h2>Center of Green Nanotechnology Innovations for Environmental Solutions (CGNIES)</h2>
+                        <h2>Coleoptera Research Center</h2>
                         <p class="font-weight-light text-justify">
                             BACKEND
                         </p>
