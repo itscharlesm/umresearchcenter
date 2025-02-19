c21132231386cd2d07819d1a45a5d3702cec10b4
itscharlesm <itscharlesm@gmail.com>
2025-02-19 10:56:58 +0800

    admin messages second partial

--- resources/views/admin/messages.blade.php
+++ resources/views/admin/messages.blade.php
@@ -24,28 +24,35 @@
         <div class="container-fluid">
             @include('layouts.partials.alerts')
             <div class="card">
+                <div class="card-header bg-warning">
+                    List of users
+                </div>
                 <div class="card-body overflow-auto">
                     <table id="studentTable" class="table table-hover table-striped table-sm responsive">
                         <thead>
                             <tr>
+                                <th style="text-align: center; vertical-align: middle">Date</th>
                                 <th style="text-align: center; vertical-align: middle">Name</th>
                                 <th style="text-align: center; vertical-align: middle">Number</th>
                                 <th style="text-align: center; vertical-align: middle">Email</th>
-                                <th style="text-align: center; vertical-align: middle">Message</th>
+                                <th style="text-align: center; vertical-align: middle">Status</th>
                                 <th style="text-align: center; vertical-align: middle; width: 100px">Action</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($messages as $con)
-                                <tr style="text-align: justify; vertical-align: middle;">
+                                <tr style="text-align: center; vertical-align: middle;">
+                                    <td style="vertical-align: middle;">
+                                        {{ \Carbon\Carbon::parse($con->con_date_created)->format('F j, Y | g:i A') }}
+                                    </td>                                    
                                     <td style="vertical-align: middle;">{{ $con->con_name }}</td>
                                     <td style="vertical-align: middle;">{{ $con->con_number }}</td>
-                                    <td style="vertical-align: middle;">{{ $con->con_email }}</td>
-                                    <td style="vertical-align: middle;">{{ $con->con_message }}</td>
+                                    <td style="vertical-align: middle;">{{ $con->con_email}}</td>
+                                    <td style="vertical-align: middle;">{{ $con->con_status }}</td>
                                     <td style="vertical-align: middle;">
                                         <a class="btn btn-warning btn-sm activate-btn" href="javascript:void(0)"
                                             data-toggle="modal" data-target="#updateDescriptionModal-">
-                                            <span class="fa fa-edit"></span> Update
+                                            <span class="fa fa-eye"></span> View
                                         </a>
                                     </td>
                                 </tr>
