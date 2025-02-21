890c2a9035e74237f35bf71c3fe386a35dbd4e5c
itscharlesm <itscharlesm@gmail.com>
2025-02-21 11:24:14 +0800

    carousel front end done

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use DB;
 
 class MainController extends Controller
 {
     public function main()
     {
-        return view('index');
+        $carousel = DB::table('carousel')
+            ->where('cour_active', 1)
+            ->get();
+
+        return view('index', compact('carousel'));
     }
 }
--- resources/views/main/carousel.blade.php
+++ resources/views/main/carousel.blade.php
@@ -1,51 +1,25 @@
 <section class="hero-section overlay bg-cover" data-background="{{ asset('images/bg.webp') }}">
     <div class="container">
         <div class="hero-slider" style="max-height: 400px; display: flex">
-            <!-- slider item -->
-            <div class="hero-slider-item">
-                <div class="row">
-                    <div class="col-md-12">
-                        <center>
-                            <div class="hero-slider-item" style="display: flex; justify-content: center; align-items: center; width: 100%; height: 400px;">
-                                <div class="image-container" style="width: 100%; height: 400px; display: flex; justify-content: center; align-items: center; overflow: hidden;">
-                                    <img src="{{ asset('images/carousel/1.jpg') }}" alt="carousel-item-1" 
-                                         class="img-fluid" style="width: 100%; height: 100%; object-fit: cover;">
+            @foreach ($carousel as $cour)
+                <div class="hero-slider-item">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <center>
+                                <div class="hero-slider-item"
+                                    style="display: flex; justify-content: center; align-items: center; width: 100%; height: 400px;">
+                                    <div class="image-container"
+                                        style="width: 100%; height: 400px; display: flex; justify-content: center; align-items: center; overflow: hidden;">
+                                        <img src="{{ asset('images/carousel/' . $cour->cour_image) }}"
+                                            alt="carousel-item-1" class="img-fluid"
+                                            style="width: 100%; height: 100%; object-fit: cover;">
+                                    </div>
                                 </div>
-                            </div>
-                        </center>
+                            </center>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- slider item -->
-            <div class="hero-slider-item">
-                <div class="row">
-                    <div class="col-md-12">
-                        <center>
-                            <div class="hero-slider-item" style="display: flex; justify-content: center; align-items: center; width: 100%; height: 400px;">
-                                <div class="image-container" style="width: 100%; height: 400px; display: flex; justify-content: center; align-items: center; overflow: hidden;">
-                                    <img src="{{ asset('images/carousel/2.jpg') }}" alt="carousel-item-2" 
-                                         class="img-fluid" style="width: 100%; height: 100%; object-fit: cover;">
-                                </div>
-                            </div>
-                        </center>
-                    </div>
-                </div>
-            </div>
-            <!-- slider item -->
-            <div class="hero-slider-item">
-                <div class="row">
-                    <div class="col-md-12">
-                        <center>
-                            <div class="hero-slider-item" style="display: flex; justify-content: center; align-items: center; width: 100%; height: 400px;">
-                                <div class="image-container" style="width: 100%; height: 400px; display: flex; justify-content: center; align-items: center; overflow: hidden;">
-                                    <img src="{{ asset('images/carousel/3.jpg') }}" alt="carousel-item-3" 
-                                         class="img-fluid" style="width: 100%; height: 100%; object-fit: cover;">
-                                </div>
-                            </div>
-                        </center>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </section>
