aadf2b86a0f047d2bdd226ca331728253a01b6b2
itscharlesm <itscharlesm@gmail.com>
2025-02-24 14:57:55 +0800

    admin social done

--- resources/views/admin/journal/social.blade.php
+++ resources/views/admin/journal/social.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">JOURNAL OF TROPICAL COLEOPTEROLOGY (JTC)</h1>
+                    <h1 class="m-0">JOURNAL OF SOCIAL WELFARE AND HUMAN DEVELOPMENT STUDIES (JSWHDS) - UPCOMING</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -13,7 +13,7 @@
                             <a href="{{ action('App\Http\Controllers\AdminController@home') }}">Home</a>
                         </li>
                         <li class="breadcrumb-item active">Journals</li>
-                        <li class="breadcrumb-item active">JTC</li>
+                        <li class="breadcrumb-item active">JSWHDS</li>
                     </ol>
                 </div>
             </div>
