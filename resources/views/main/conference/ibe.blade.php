6b78b9b4c248c8fb5130fa24b3367ee18144521f
itscharlesm <itscharlesm@gmail.com>
2025-02-24 17:27:04 +0800

    ibe done

---
+++ resources/views/main/conference/ibe.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.themes.main')
+@section('content')
+    <section class="section bg-grey">
+        <div class="container lg-10">
+            @include('layouts.partials.alerts')
+            <!-- Title -->
+            <div class="content-header text-center" data-aos="zoom-in-left" data-aos-duration="1000" data-aos-delay="100"
+                data-aos-once="false" style="margin-top: 100px;">
+                <x-content-header title="CONFERENCE BY IBE" />
+            </div>
+            <!-- Breadcrumb -->
+            <x-breadcrumb :items="[
+                ['label' => 'Home', 'url' => url('/'), 'active' => false],
+                ['label' => 'Conference', 'url' => '#', 'active' => true],
+                ['label' => 'IBE', 'url' => '#', 'active' => true],
+            ]" />
+            <div class="row">
+                <!-- Attachments -->
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h2>Conference by IBE</h2>
+                        <div>
+                            {!! $ibe_description !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
