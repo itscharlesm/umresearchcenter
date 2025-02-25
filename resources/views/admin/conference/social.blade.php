e28f2df283c88f94754d8b2de87613a05203bdb6
itscharlesm <itscharlesm@gmail.com>
2025-02-25 10:43:57 +0800

    admin social second partial

--- resources/views/admin/conference/social.blade.php
+++ resources/views/admin/conference/social.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">RESEARCH CONFERENCE ON ECONOMY AND ENTERPRISE STUDIES</h1>
+                    <h1 class="m-0">RESEARCH CONFERENCE ON SOCIAL WELFARE AND HUMAN DEVELOPMENT STUDIES</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -13,7 +13,7 @@
                             <a href="{{ action('App\Http\Controllers\AdminController@home') }}">Home</a>
                         </li>
                         <li class="breadcrumb-item active">Conferences</li>
-                        <li class="breadcrumb-item active">Economy and Enterprise Studies</li>
+                        <li class="breadcrumb-item active">Social Welfare and Human Development Studies</li>
                     </ol>
                 </div>
             </div>
