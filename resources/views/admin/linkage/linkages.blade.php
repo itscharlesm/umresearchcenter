699925c9fbadbec8f4448ab96c4aa8afb6495472
itscharlesm <itscharlesm@gmail.com>
2025-02-28 10:44:04 +0800

    hyperlink added in linkage

--- resources/views/admin/linkage/linkages.blade.php
+++ resources/views/admin/linkage/linkages.blade.php
@@ -144,6 +144,7 @@
                     ['font', ['strikethrough', 'superscript', 'subscript']],
                     ['color', ['color']],
                     ['para', ['ul', 'ol', 'paragraph']],
+                    ['insert', ['link']],
                     ['height', ['height']]
                 ],
                 callbacks: {
