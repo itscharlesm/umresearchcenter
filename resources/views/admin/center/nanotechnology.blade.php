aa5678eccc175b569e8d150b9204cfe6948cd7f0
itscharlesm <itscharlesm@gmail.com>
2025-02-28 10:40:21 +0800

    hyperlink added in center

--- resources/views/admin/center/nanotechnology.blade.php
+++ resources/views/admin/center/nanotechnology.blade.php
@@ -92,6 +92,7 @@
                     ['font', ['strikethrough', 'superscript', 'subscript']],
                     ['color', ['color']],
                     ['para', ['ul', 'ol', 'paragraph']],
+                    ['insert', ['link']],
                     ['height', ['height']]
                 ],
                 callbacks: {
